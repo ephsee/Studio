e60aa82eb3973b5c919d2b8767cd36ebdb69ffe5
Francois Spies <fcspies@gmail.com>
2022-04-07 13:44:07 -0400

    active storage access

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  useEffect(()=>{
+    fetch('/posts')
+    .then(r=>r.json())
+    .then(setPosts)
+}, [])
+
+const [content, setContent] = useState("")
+const [posts, setPosts] = useState([])
+
+console.log(posts)
+console.log(content)
+
+// const showPosts = posts.map( p => <div key={p.id}>{p.info}</div>)
+
+// const [show, setShow] = useState({})
+
+// console.log(show)
+
+function handleContent(e){
+    console.log(e.target.files[0])
+    setContent(e.target.files[0])
+    // setShow(e.target.value)
+}
+
+function handleSubmit(){
+
+    // e.PreventDefault()
+
+    const formData = new FormData()
+    // formData.append('user_id', 1)
+    formData.append('content', "something brand new upload")
+    formData.append('featured_image', content)
+
+    // const newPost = {
+    //     upload: content,
+    //     info: "something new upload",
+    //     user_id: 1
+    // }
+
+    // debugger
+
+    console.log(formData)
+
+    // http://127.0.0.1:3000/
+
+    fetch('/posts',{
+        method:'POST',
+        // headers:{'Content-Type': 'application/json'},
+        // headers:{'Content-Type': 'multipart/form-data'},
+        // headers:{'Content-Type': 'image'},
+        // 'Content-Type', 'text/xml'
+        // headers:{'Authorization': localStorage.token},
+        // body: JSON.stringify(formData)
+        // body: JSON.stringify(newPost)
+        body: formData
+      })
+      .then(r => r.json())
+      .then(console.log)   
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+
+            hello!!!
+
+            <form>
+                <input onChange={handleContent} type="file" accept="image/*" name="file"></input>
+                <input onClick={handleSubmit} type="submit" name="Submit"></input>
+            </form>
+      
     </div>
   );
 }
---
+++ db/schema.rb
@@ -0,0 +1,112 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_04_07_172016) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "allies", force: :cascade do |t|
+    t.string "name"
+    t.string "alter_ego"
+    t.string "city"
+    t.string "image"
+    t.bigint "hero_id"
+    t.bigint "location_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hero_id"], name: "index_allies_on_hero_id"
+    t.index ["location_id"], name: "index_allies_on_location_id"
+  end
+
+  create_table "gadgets", force: :cascade do |t|
+    t.string "item_name"
+    t.integer "quantity"
+    t.string "image"
+    t.bigint "hero_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hero_id"], name: "index_gadgets_on_hero_id"
+  end
+
+  create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "alter_ego"
+    t.string "base"
+    t.string "image"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "villains", force: :cascade do |t|
+    t.string "name"
+    t.string "alter_ego"
+    t.boolean "at_large"
+    t.integer "most_wanted"
+    t.string "notes"
+    t.string "image"
+    t.bigint "hero_id", null: false
+    t.bigint "location_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hero_id"], name: "index_villains_on_hero_id"
+    t.index ["location_id"], name: "index_villains_on_location_id"
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "gadgets", "heros"
+  add_foreign_key "villains", "heros"
+  add_foreign_key "villains", "locations"
+end
