ce924b0814e39c1e2b673ab2c8c8547c4768707a
Francois Spies <fcspies@gmail.com>
2022-04-07 19:48:22 -0400

    test

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -72,7 +72,7 @@ function handleSubmit(){
     return (
     <div>
 
-            Post - test to see if github is working
+            Post - test to see if github is working help help
 
             <form>
                 <input onChange={handleContent} type="file" accept="image/*" name="file"></input>
